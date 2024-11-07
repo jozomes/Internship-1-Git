e0007157299d9ce1050146a8240c40406579dde1
Jozo Meštrović <jozomestrovic0@gmail.com>
2024-11-07 14:57:58 +0100

    commit zadatka4 na granu4

---
+++ Dijamant/Dijamant/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class Diamond
+{
+    public static void Main()
+    {
+        int n;
+        do
+        {
+            n = int.Parse(Console.ReadLine());
+        } while (n < 0);
+        for (int i = 1; i <= n; i++)
+        {
+            
+            for (int j = 1; j <= n - i; j++)
+            {
+                Console.Write(" ");
+            }
+
+           
+            for (int j = 1; j <= 2 * i - 1; j++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+
+        
+        for (int i = n - 1; i >= 1; i--)
+        {
+            
+            for (int j = 1; j <= n - i; j++)
+            {
+                Console.Write(" ");
+            }
+
+            
+            for (int j = 1; j <= 2 * i - 1; j++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
